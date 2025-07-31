0799103ad4d9e0ff1cac4cb6668cfaf46ead41e4
ASAPROCKY2 <c3125024@gmail.com>
2025-07-31 10:59:07 +0300

    Update API endpoint in AdminDashboard to use production URL

--- src/components/dashboard/AdminDashboard/AdminDashboard.tsx
+++ src/components/dashboard/AdminDashboard/AdminDashboard.tsx
@@ -34,7 +34,7 @@ const AdminDashboard = () => {
   useEffect(() => {
     const fetchStats = async () => {
       try {
-        const res = await fetch("http://localhost:8081/admin/dashboard-stats");
+        const res = await fetch("https://hospital-management-mdbf.onrender.com/dashboard-stats");
         if (!res.ok) throw new Error("Failed to fetch stats");
         const data = await res.json();
         setStatsData(data);
